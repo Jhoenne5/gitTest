f85fe35de60f15b3497fa88acc62e60b913ac282
Jhoenne_Estrecho <jhoenneestrecho12@gmail.com>
2022-09-14 22:05:54 +0800

    first commit

---
+++ index.js
@@ -0,0 +1,20 @@
+
+
+let total= sum(1,2);
+    console.log(total);
+ displayConsole(sum);
+function sum(x,y){  
+    let result = x + y;
+        return result;
+
+}
+
+
+
+function displayConsole(sum){
+    console.log(sum);
+    
+
+
+
+}
